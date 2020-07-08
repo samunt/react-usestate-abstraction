6c5a247fff04af6097bfc5673328f880304bdef9
samunt <sammy.unterman@gmail.com>
2020-07-08 11:01:16 -0400

    toggleState, pre abstraction

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Toggler from "./Toggler";
+
+function App() {
+  return (
+    <div className="App">
+      <Toggler/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Toggler.js
@@ -0,0 +1,19 @@
+import React, {useState} from 'react';
+
+function Toggler() {
+    const [isHappy, setIsHappy] = useState(true);
+    const [isHeartbroken, setIsHeartbroken] = useState(false);
+    const toggleIsHappy = () => {
+        setIsHappy(!isHappy)
+    };
+    const toggleIsHeartbroken = () => {
+        setIsHeartbroken(!isHeartbroken)
+    };
+    return (
+        <div>IsHappy
+            <h1 onClick={toggleIsHappy}>{isHappy ? "😁":"😥"}></h1>
+            <h1 onClick={toggleIsHeartbroken}>{isHeartbroken? "💔": "💗"}></h1>
+        </div>
+    )
+}
+export default Toggler;
