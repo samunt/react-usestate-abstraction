5dbad17af0d18be86356dbbe46cf088eb1274d99
samunt <sammy.unterman@gmail.com>
2020-07-13 11:14:08 -0400

    entire hooks project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Toggler from "./Toggler";
-
+import CounterClass from './CounterClass';
+import CounterHooks from './hooks/CounterHooks.js'
 function App() {
   return (
     <div className="App">
-      <Toggler/>
+        <CounterClass/>
+        <CounterHooks/>
+        <Toggler/>
     </div>
   );
 }
---
+++ src/CounterClass.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+
+export default class CounterClass extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            count: 0
+        };
+        this.increment = this.increment.bind(this)
+    }
+    increment() {
+        this.setState({count: this.state.count + 1})
+    }
+    render() {
+        return (
+            <div>
+                <h1>The count is {this.state.count}</h1>
+                <button onClick={this.increment}>INCREMENT</button>
+            </div>
+        )
+    }
+}
